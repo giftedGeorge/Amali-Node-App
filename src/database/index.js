c15e778b06ac7ceb83a2a5328e985aea25ee8602
Ejiawoko George <ejiawokogeorge@gmail.com>
2023-09-11 22:23:07 +0100

    fixed db connect bug

--- src/database/index.js
+++ src/database/index.js
@@ -5,8 +5,6 @@ async function ConnectToDb(){
     try{
         return mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser:true,
-            useCreateIndex:true,
-            useFindAndModify:false,
             useUnifiedTopology:true,
         });
     } catch(e){
