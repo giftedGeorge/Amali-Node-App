a55bf60c4a42554eb1dba6b225dcb140f7927fce
Ejiawoko George <ejiawokogeorge@gmail.com>
2023-09-20 14:47:10 +0100

    minor change

--- src/models/signUp.js
+++ src/models/signUp.js
@@ -7,7 +7,7 @@ const signUpSchema = new Schema({
     UserType: {type: Number, required: [true, 'type of user must be provided'], trim: true},
     IsInterestAllowed: {type: Boolean, default: true},
     IsPhoneNumberVerified: {type: Boolean, default: false},
-    CreatedAt: {type: Date, default: Date.now}
+    CreatedAt: {type: Date, default: Date.now},
     ModifiedAt: {type: Date, default: Date.now}
 });
 
